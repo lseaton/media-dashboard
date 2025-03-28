5869564129ed53007bab6ce1c3987ab0b6e35317
lseaton <73501741+lseaton@users.noreply.github.com>
2025-03-28 10:07:54 -0700

    Change card animation and add javascript file

---
+++ media-dashboard.js
@@ -0,0 +1,20 @@
+let cardImgs = document.querySelectorAll(".card img");
+console.log(cardImgs);
+for (let i = 0; i < cardImgs.length; i++) {
+	cardImgs[i].addEventListener("mouseover", revealInfo);
+	cardImgs[i].addEventListener("mouseleave", hideInfo);
+}
+
+function revealInfo(e) {
+	e.target.style.animation = "three-quarters-fade-out 1s";
+	e.target.style.opacity = "25%";
+	e.target.nextElementSibling.style.animation = "fade-in 2s";
+	e.target.nextElementSibling.style.opacity = "100%";
+}
+
+function hideInfo(e) {
+	e.target.nextElementSibling.style.animation = "none";
+	e.target.nextElementSibling.style.opacity = "0%";
+	e.target.style.animation = "three-quarters-fade-in 1s";
+	e.target.style.opacity = "100%";
+}
